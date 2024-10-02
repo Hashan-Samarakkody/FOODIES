180978c085f8cf5c1d57f68ede8ec39cf93681c4
Hashan Samarakkodi <samarak-im21007@stu.kln.ac.lk>
2024-10-02 12:52:30 +0530

    View Others Recipes Is Added.

--- app/src/main/java/com/example/foodies/ViewOthersRecipeActivity.java
+++ app/src/main/java/com/example/foodies/ViewOthersRecipeActivity.java
@@ -1,18 +1,97 @@
 package com.example.foodies;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewOthersRecipeActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    List<DataClass> dataList;
+    DatabaseReference databaseReference;
+    ValueEventListener eventListener;
+    ViewOthersRecipeAdapter adapter;
+    ImageView backIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_others_recipe);
+
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            // Redirect to login activity
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return;
+        }
+
+        recyclerView = findViewById(R.id.recyclerView);
+        backIcon = findViewById(R.id.back);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(ViewOthersRecipeActivity.this, 1);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(ViewOthersRecipeActivity.this);
+        builder.setCancelable(false);
+        builder.setView(R.layout.progress_layout);
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        dataList = new ArrayList<>();
+        adapter = new ViewOthersRecipeAdapter(ViewOthersRecipeActivity.this, dataList);
+        recyclerView.setAdapter(adapter);
+
+        // Set the database reference and listener
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        databaseReference = FirebaseDatabase.getInstance().getReference("Recipes");
+
+        eventListener = databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                dataList.clear();
+                for (DataSnapshot itemSnapshot : snapshot.getChildren()) {
+                    DataClass dataClass = itemSnapshot.getValue(DataClass.class);
+                    if (dataClass != null  && !dataClass.getOwner().equals(userId)) {
+                        dataClass.setKey(itemSnapshot.getKey());
+                        dataList.add(dataClass);
+                    }
+                }
+                adapter.notifyDataSetChanged();
+                dialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                dialog.dismiss();
+            }
+        });
+
+        backIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ViewOthersRecipeActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/foodies/ViewOthersRecipeAdapter.java
@@ -0,0 +1,93 @@
+package com.example.foodies;
+
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+import com.bumptech.glide.Glide;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewOthersRecipeAdapter extends RecyclerView.Adapter<ViewOthersViewHolder> {
+
+    final Context context;
+    private List<DataClass> dataList;
+
+    public ViewOthersRecipeAdapter(Context context, List<DataClass> dataList) {
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+    @NonNull
+    @Override
+    public ViewOthersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.other_recipe_item, parent, false);
+        return new ViewOthersViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewOthersViewHolder holder, int position) {
+        DataClass data = dataList.get(position);
+
+        // Load image using Glide
+        Glide.with(context)
+                .load(data.getDataImage())
+                .placeholder(R.drawable.r_2) // Optional: add a placeholder image
+                .into(holder.recImage);
+
+        // Set text views
+        holder.recTitle.setText(data.getDataName());
+        holder.recTime.setText(data.getDataTime());
+
+        // Set click listener
+        holder.recCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context, DetailActivity.class);
+                intent.putExtra("Image", data.getDataImage());
+                intent.putExtra("Name", data.getDataName());
+                intent.putExtra("Time", data.getDataTime());
+                intent.putExtra("Category", data.getDataCategory());
+                intent.putExtra("Ingredients", data.getDataIngredients());
+                intent.putExtra("Description", data.getDataDescription());
+                intent.putExtra("Key", data.getKey());
+                intent.putExtra("Owner", data.getOwner());
+
+                context.startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return dataList.size();
+    }
+
+    public void searchDataList(ArrayList<DataClass> searchList) {
+        dataList = searchList;
+        notifyDataSetChanged();
+    }
+}
+
+class ViewOthersViewHolder extends RecyclerView.ViewHolder {
+
+    ImageView recImage;
+    TextView recTitle, recTime;
+    CardView recCard;
+
+    public ViewOthersViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        recImage = itemView.findViewById(R.id.otherRecImage);
+        recTitle = itemView.findViewById(R.id.otherRecTitle);
+        recTime = itemView.findViewById(R.id.otherRecTime);
+        recCard = itemView.findViewById(R.id.otherRecCard);
+    }
+}
+
